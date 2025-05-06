ae4489ed1a4096550f558bbdf276517104d6f81f
pdotsani <psj.webdev@gmail.com>
2025-05-06 08:06:37 -0700

    to section 10

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,23 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Move(Point { x, y }) => {
+                self.move_position(Point { x, y });
+            }
+            Message::Resize { width, height } => {
+                self.resize(width, height);
+            }
+            Message::Echo(s) => {
+                self.echo(s);
+            }
+            Message::ChangeColor(red, green, blue) => {
+                self.change_color(red, green, blue);
+            }
+            Message::Quit => {
+                self.quit();
+            }   
+        };
     }
 }
 
